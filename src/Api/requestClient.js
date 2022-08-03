129187e9ae3a6b87006df10021761887155cfba8
tech2dev <110522737+tech2dev@users.noreply.github.com>
2022-08-03 16:27:12 +0700

    Update requestClient.js

--- src/Api/requestClient.js
+++ src/Api/requestClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const requestClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/tech2dev/fake-database/',
+    baseURL: 'http://localhost:3001/',
     timeout: 1000,
     headers: { 'X-Custom-Header': 'foobar' }
 });
